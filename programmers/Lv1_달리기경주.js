5465be84ae2546014cb959f5c30c78ce2b854ccb
권궁 <gwongung@gwongung-ui-MacBookPro.local>
2023-10-19 19:48:58 +0900

    programmers lv1 달리기경주

---
+++ programmers/Lv1_달리기경주.js
@@ -0,0 +1,29 @@
+function solution(players, callings) {
+    let answer = [];
+
+    const hashSet = new Map();
+
+    players.forEach((list, index) => {
+        hashSet.set(list, index);
+    })
+
+    callings.forEach(list => {
+        const currentIndex = hashSet.get(list);
+        const prevKey = players[currentIndex - 1];
+
+        players[currentIndex - 1] = players[currentIndex];
+        players[currentIndex] = prevKey;
+        hashSet.set(list, currentIndex - 1);
+        hashSet.set(prevKey, currentIndex);
+    })
+    
+    console.log('PLAy', players);
+
+    answer = players;
+    return answer;
+}
+
+
+const players = ["mumu", "soe", "poe", "kai", "mine"];
+const callings = ["kai", "kai", "mine", "mine"];
+console.log( 'RESULT ', solution(players, callings))
